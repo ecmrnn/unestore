e34130a4d99954371dcffa866029c7a8eccb927c
Ec Maranan <marananemanuelle@gmail.com>
2022-02-17 01:49:57 +0800

    Add error message when product list is empty

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,21 @@
 
 function searchProduct() {
     // Declaring Variables
-    var input, list, product;
+    var input, list, product, empty, test, i;
     input = document.getElementById("searchInput").value.toLowerCase();
     list = document.getElementsByClassName("product");
+    empty = document.getElementsByClassName("product__empty");
 
     for (i = 0; i < list.length; i++) {
         product = list[i].getElementsByClassName("product__title")[0];
         if (product.innerHTML.toLowerCase().indexOf(input) > -1) {
             list[i].style.display = "";
+            test = true;
         } else {
             list[i].style.display = "none";
+        }
+        if (i == list.length - 1 && test != true) { 
+            empty[0].style.display = "flex";
         }
     }
 }
